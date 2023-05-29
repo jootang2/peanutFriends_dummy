9a2407c4bd6b968bc0b06bcefa80e54a3bea80bb
joohwan9607 <joohwan9607@mobility42.io>
2023-05-29 15:36:13 +0900

    create basket front

--- peanutfriend_front/src/pages/AddBasket.vue
+++ peanutfriend_front/src/pages/AddBasket.vue
@@ -22,52 +22,33 @@
               <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                  <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
                   <div class="col-lg-7">
                     <div class="p-5">
                       <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                        <h1 class="h4 text-gray-900 mb-4">Create an Peanut Basket!</h1>
                       </div>
                       <form class="user">
                         <div class="form-group row">
-                          <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                   placeholder="First Name">
-                          </div>
-                          <div class="col-sm-6">
-                            <input type="text" class="form-control form-control-user" id="exampleLastName"
-                                   placeholder="Last Name">
-                          </div>
                         </div>
                         <div class="form-group">
-                          <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                                 placeholder="Email Address">
+                          <input type="text" class="form-control form-control-user" id="basketName"
+                                 placeholder="바스켓 이름" v-model="state.form.name">
                         </div>
                         <div class="form-group row">
                           <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="password" class="form-control form-control-user"
-                                   id="exampleInputPassword" placeholder="Password">
+                            <input type="text" class="form-control form-control-user"
+                                   id="basketStartDate" placeholder="시작 날짜" v-model="state.form.startDate">
                           </div>
                           <div class="col-sm-6">
-                            <input type="password" class="form-control form-control-user"
-                                   id="exampleRepeatPassword" placeholder="Repeat Password">
+                            <input type="text" class="form-control form-control-user"
+                                   id="basketEndDate" placeholder="끝 나는 날짜" v-model="state.form.endDate">
                           </div>
                         </div>
-                        <a href="login.html" class="btn btn-primary btn-user btn-block">
-                          Register Account
-                        </a>
-                        <hr>
-                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                          <i class="fab fa-google fa-fw"></i> Register with Google
-                        </a>
-                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                          <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                        <a class="btn btn-primary btn-user btn-block" @click="submit()">
+                          create Basket
                         </a>
                       </form>
                       <hr>
-                      <div class="text-center">
-                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                      </div>
                       <div class="text-center">
                         <a class="small" href="login.html">Already have an account? Login!</a>
                       </div>
@@ -99,8 +80,31 @@
 import LogoutModal from "@/components/LogoutModal.vue";
 import TopBar from "@/components/TopBar.vue";
 import Lnb from "@/components/LNB.vue";
+import {reactive} from "vue";
+import axios from "axios";
+import router from "@/scripts/router";
 
 export default {
+  setup() {
+    const state = reactive({
+      form: {
+        name: "",
+        startDate: "",
+        endDate: ""
+      }
+    })
+
+    const submit = () => {
+      axios.post("/api/basket/create", state.form).then((res) => {
+        console.log(res)
+        router.push({path: "/"})
+      }).catch(() => {
+        window.alert("바스켓 생성에 실패했습니다.")
+      })
+    }
+
+    return {state, submit}
+  },
   name: "AddBasket",
   components: {Lnb, TopBar, LogoutModal}
 }
