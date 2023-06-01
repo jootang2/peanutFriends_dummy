b880e0406fd25cf099aad44f3559a0e023e0e561
joohwan <joohwan9607@mobility42.io>
2023-06-01 19:50:22 +0900

    basketController testcode - 1

---
+++ src/test/java/com/example/peanutfriends_0505/controller/BasketControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.peanutfriends_0505.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BasketControllerTest {
+
+    @Test
+    void addBasket() {
+    }
+
+    @Test
+    void getBaskets() {
+    }
+
+    @Test
+    void signUpBasket() {
+    }
+}
